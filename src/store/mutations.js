f6325877dcf3f33fae3ef08bdec9ae7996714737
Juan Martinez <1101462jucamajr@gmail.com>
2025-11-30 15:06:59 -0600

    Data Store and Meals by Letter complete

---
+++ src/store/mutations.js
@@ -0,0 +1,11 @@
+export function setSearchedMeals(state, meals) {
+    state.searchedMeals = meals || []
+}
+
+export function setMealsByLetter(state, meals) {
+    state.MealsByLetter = meals || []
+}
+
+export function setMealsByIngredients(state, meals) {
+    state.MealsByIngredient = meals || []
+}
